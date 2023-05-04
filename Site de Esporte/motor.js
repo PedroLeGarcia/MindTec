c5aeb3864fdd9faac70db63a4f109c7e8618098b
Werner <117999488+w4rxz@users.noreply.github.com>
2023-05-03 21:18:50 -0300

    Delete Site de Esporte directory

--- Site de Esporte/motor.js
+++
@@ -1,6 +0,0 @@
-var btnExp = document.querySelector('#btn-exp')
-var menuSide = document.querySelector('.menu-lateral')
-
-btnExp.addEventListener('click', function(){
-    menuSide.classList.toggle('expandir')
-})
